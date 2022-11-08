cb798022f477a442bad5c6cd4844bc7b2eec87f0
maruf <dev.maruf112@gmail.com>
2022-11-08 20:34:49 +0600

    limit services on home

---
+++ index.js
@@ -0,0 +1,47 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+// port
+const port = process.env.PORT;
+// middleware
+app.use(cors());
+app.use(express.json());
+// app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("I am running on the home.");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.efpjwcu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const serviceCollection = client
+      .db("servicesDatabase")
+      .collection("services");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    app.get("/servicesLimit", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.log(err));
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
